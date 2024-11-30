e48125a023dd94a37ac788c9a9d5dbfc51cea24a
josemaripangilinan <josemaripangilinan@gmail.com>
2024-11-30 08:56:34 +0800

    first commit

---
+++ weatherapp/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.contrib import messages
+import requests
+import datetime
+
+
+def home(request):
+
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'manila'     
+    
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=56e96e5f1876ae9097a4a83cc039b977'
+    PARAMS = {'units':'metric'}
+
+    API_KEY =  'AIzaSyBYBtsOUiqCvbUHOsMpwc74AMAqJiAMiNo'
+    
+
+    SEARCH_ENGINE_ID = '43f82b34444a74fce'
+     
+    query = city + " 1920x1080"
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+
+    data = requests.get(city_url).json()
+    count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
+    
+
+    try:
+          
+          data = requests.get(url,params=PARAMS).json()
+          description = data['weather'][0]['description']
+          icon = data['weather'][0]['icon']
+          temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'weatherapp/index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
+    
+    except KeyError:
+          exception_occurred = True
+          messages.error(request,'Entered data is not available to API')   
+          # city = 'indore'
+          # data = requests.get(url,params=PARAMS).json()
+          
+          # description = data['weather'][0]['description']
+          # icon = data['weather'][0]['icon']
+          # temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'weatherapp/index.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'manila' , 'exception_occurred':exception_occurred } )
+               
+    
+    
